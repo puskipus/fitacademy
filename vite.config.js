9a894978f091ef4eb3817bb4054b5e1c5eaab471
puskipus <thoriqachmad.r@gmail.com>
2023-03-08 04:22:15 +0000

    tes

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,10 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    plugins: [
+server: {
+        host: '139.162.38.133',  // Add this to force IPv4 only
+    },
+plugins: [
         laravel({
             input: [
                 'resources/css/app.css',
