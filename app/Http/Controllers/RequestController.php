2dcf8d6978be67c21ca30b818ffc098eea4dd473
Thoriq Achmad <thoriqachmad.r@gmail.com>
2023-03-10 14:22:26 +0700

    kirim email

--- app/Http/Controllers/RequestController.php
+++ app/Http/Controllers/RequestController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Request as ModelsRequest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class RequestController extends Controller
 {
@@ -14,7 +15,25 @@ class RequestController extends Controller
             'jenis' => "perusahaan",
         ];
 
+        $email["email"] = $request->email;
+        $email["title"] = "From admin";
+        $email["body"] = "ini isi";
+
+        $files = [
+            public_path('files/Employee 101 - Cara Ampuh dan Efektif Meningkatkan Kinerja Karyawan.pdf'),
+        ];
+
+        Mail::send('landingPage.landingPerusahaan', $email, function($message)use($email, $files) {
+            $message->to($email["email"], $email["email"])
+                    ->subject($email["title"]);
+
+            foreach ($files as $file){
+                $message->attach($file);
+            }
+
+        });
+
         ModelsRequest::create($data);
-        return redirect()-back();
+        dd('Mail sent successfully');
     }
 }
