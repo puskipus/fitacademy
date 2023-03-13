3eea165034432b5c21193e2d01db431be7790b41
Thoriq Achmad <thoriqachmad.r@gmail.com>
2023-03-13 09:30:41 +0700

    fix duplikasi data email, kirim ebook content dan netizen

--- app/Http/Controllers/RequestController.php
+++ app/Http/Controllers/RequestController.php
@@ -8,20 +8,38 @@ use Illuminate\Support\Facades\Mail;
 
 class RequestController extends Controller
 {
-    public function addRequestPerusahaan(Request $request)
+    // controller untuk mengirim ebook dan menyimpan email ke database
+    public function addRequest(Request $request)
     {
         $data = [
             'email' => $request->email,
-            'jenis' => "perusahaan",
+            'jenis' => $request->jenis,
         ];
 
         $email["email"] = $request->email;
         $email["title"] = "From admin";
         $email["body"] = "ini isi";
 
-        $files = [
-            public_path('files/Employee 101 - Cara Ampuh dan Efektif Meningkatkan Kinerja Karyawan.pdf'),
-        ];
+        switch ($data["jenis"]) {
+            case "perusahaan":
+                $files = [
+                    public_path('files/Employee 101 - Cara Ampuh dan Efektif Meningkatkan Kinerja Karyawan.pdf'),
+                ];
+                break;
+            case "contentCreator":
+                $files = [
+                    public_path('files/Content Creator Guide - Asik Mengedukasi dengan Metode Microlearning.pdf'),
+                ];
+                break;
+            case "netizen":
+                $files = [
+                    public_path('files/Career Path - Sukses Menjadi Seorang Copywriter.pdf'),
+                ];
+                break;
+            default:
+                dd("jenis tidak ada");
+                break;
+        }
 
         Mail::send('landingPage.landingPerusahaan', $email, function($message)use($email, $files) {
             $message->to($email["email"], $email["email"])
@@ -33,7 +51,12 @@ class RequestController extends Controller
 
         });
 
-        ModelsRequest::create($data);
-        dd('Mail sent successfully');
+        if(ModelsRequest::where('email', $request->email)->touch() && ModelsRequest::where('email', $request->jenis)->touch()) {
+            return redirect()->back();
+        } else {
+            ModelsRequest::create($data);
+        }
+
+        return redirect()->back();
     }
 }
