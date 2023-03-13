a313bbae3a29b41b2e7a621c6eb528698120c9e3
Thoriq Achmad <thoriqachmad.r@gmail.com>
2023-03-13 16:24:48 +0700

    memperbaiki landing page

--- resources/views/landingPage/master.blade.php
+++ resources/views/landingPage/master.blade.php
@@ -4,11 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" href="image/Fit Academy 2022 Logo PNG.png" type="image/icon type">
     @vite('resources/css/app.css')
     <title>FitAcademy</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Nunito+Sans:wght@400;700&family=Poppins:wght@400;500;600&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Nunito+Sans:wght@400;700&family=Poppins:wght@400;500;600&family=Public+Sans:wght@400;500;600;700&display=swap" rel="stylesheet">
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <!-- component -->
