eb74757853eeb546d4022d48aafdb6db152d8c02
DestaAri1 <destacya@gmail.com>
2023-03-14 10:58:51 +0700

    email

--- app/Http/Controllers/RequestController.php
+++ app/Http/Controllers/RequestController.php
@@ -17,41 +17,41 @@ class RequestController extends Controller
         ];
 
         $email["email"] = $request->email;
-        $email["title"] = "From admin";
-        $email["body"] = "ini isi";
+        $email["title"] = "Ada Kiriman dari FitAcademy Spesial untuk Anda!";
 
         switch ($data["jenis"]) {
             case "perusahaan":
                 $files = [
                     public_path('files/Employee 101 - Cara Ampuh dan Efektif Meningkatkan Kinerja Karyawan.pdf'),
                 ];
+                $email["gambar"] = '/image/MOCKUP EBOOK 1.png';
                 break;
             case "contentCreator":
                 $files = [
                     public_path('files/Content Creator Guide - Asik Mengedukasi dengan Metode Microlearning.pdf'),
                 ];
+                $email["gambar"] = '/image/MOCKUP EBOOK 2.png';
                 break;
             case "netizen":
                 $files = [
                     public_path('files/Career Path - Sukses Menjadi Seorang Copywriter.pdf'),
                 ];
+                $email["gambar"] = '/image/MOCKUP EBOOK 3.png';
                 break;
             default:
                 dd("jenis tidak ada");
                 break;
         }
 
-        Mail::send('landingPage.landingPerusahaan', $email, function($message)use($email, $files) {
+        Mail::send('emails.email', $email, function($message)use($email, $files) {
             $message->to($email["email"], $email["email"])
                     ->subject($email["title"]);
-
             foreach ($files as $file){
                 $message->attach($file);
             }
-
         });
 
-        if(ModelsRequest::where('email', $request->email)->touch() && ModelsRequest::where('email', $request->jenis)->touch()) {
+        if(ModelsRequest::where('email', $request->email)->touch() && ModelsRequest::where('jenis', $request->jenis)->touch()) {
             return redirect()->back();
         } else {
             ModelsRequest::create($data);
---
+++ resources/views/emails/email.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>FitAcademy</title>
+</head>
+<body>
+    <img src="{{ $message->embed(public_path().$gambar) }}">
+    <p>Selamat</p>
+    {{-- <p>E-Book <b>"{{ dsa }}"</b></p> --}}
+</body>
+</html>
